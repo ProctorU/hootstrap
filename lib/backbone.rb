6aef3e6a953b94685fabb14aec71a88e88009598
Justin Licata <licata.justin@gmail.com>
2017-10-17 18:13:27 -0500

    Remove bootstrap path

--- lib/backbone.rb
+++ lib/backbone.rb
@@ -8,13 +8,13 @@ module Backbone
     asset_root = File.expand_path('../../assets', __FILE__)
     stylesheets_path  = File.join(asset_root, 'stylesheets')
 
-    bootstrap = Gem::Specification.find_by_name('bootstrap')
-    bootstrap_root_path = bootstrap.gem_dir
+    # bootstrap = Gem::Specification.find_by_name('bootstrap')
+    # bootstrap_root_path = bootstrap.gem_dir
 
     Sass.load_paths << stylesheets_path
-    Sass.load_paths << bootstrap_root_path
+    # Sass.load_paths << bootstrap_root_path
     Sprockets.append_path(stylesheets_path)
-    Sprockets.append_path(bootstrap_root_path)
+    # Sprockets.append_path(bootstrap_root_path)
   end
 end
 
