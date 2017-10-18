70934c06b49ef0ba4a40598e3f8e5fc034a1eb81
Justin Licata <licata.justin@gmail.com>
2017-10-17 21:09:30 -0500

    Add modal JS

--- lib/nest.rb
+++ lib/nest.rb
@@ -7,6 +7,7 @@ module Nest
   def self.load
     asset_root = File.expand_path('../../assets', __FILE__)
     stylesheets_path  = File.join(asset_root, 'stylesheets')
+    javascripts_path  = File.join(asset_root, 'javascripts')
 
     # bootstrap = Gem::Specification.find_by_name('bootstrap')
     # bootstrap_root_path = bootstrap.gem_dir
@@ -14,6 +15,7 @@ module Nest
     Sass.load_paths << stylesheets_path
     # Sass.load_paths << bootstrap_root_path
     Sprockets.append_path(stylesheets_path)
+    Sprockets.append_path(javascripts_path)
     # Sprockets.append_path(bootstrap_root_path)
   end
 end
