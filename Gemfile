174a5c3209097c6b6bf210b7928b4b07d82bc4ef
Justin Licata <licata.justin@gmail.com>
2017-10-18 19:14:37 -0500

    Remove old code

--- Gemfile
+++ Gemfile
@@ -11,5 +11,4 @@ gemspec
 # your gem to rubygems.org.
 
 # To use a debugger
-gem 'byebug', group: [:development, :test]
-gem 'bootstrap', '~> 4.0.0.beta'
+# gem 'byebug', group: [:development, :test]
--- lib/nest.rb
+++ lib/nest.rb
@@ -9,14 +9,9 @@ module Nest
     stylesheets_path  = File.join(asset_root, 'stylesheets')
     javascripts_path  = File.join(asset_root, 'javascripts')
 
-    # bootstrap = Gem::Specification.find_by_name('bootstrap')
-    # bootstrap_root_path = bootstrap.gem_dir
-
     Sass.load_paths << stylesheets_path
-    # Sass.load_paths << bootstrap_root_path
     Sprockets.append_path(stylesheets_path)
     Sprockets.append_path(javascripts_path)
-    # Sprockets.append_path(bootstrap_root_path)
   end
 end
 
